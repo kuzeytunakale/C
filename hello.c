436416aa1633e44dd7ff6b6886073b0dcb1860a9
Kuzey T. Kale <166996695+kuzeytunakale@users.noreply.github.com>
2025-07-19 17:37:24 +0000

    end

--- hello.c
+++ hello.c
@@ -2,15 +2,21 @@
 #include <math.h>
 
 int main() {
-    int number = 1234, a = number, basamak = 0;
+    int number = 1234, a = number, basamak = 0, sum = 0;
     while (a != 0) {
         a /= 10;
         basamak++;
     }
 
     for (int i = 0; i < basamak; i++)
-	{ 
+	{ /*
         printf("\n%d\n", (number % ((int)pow(10, (basamak - i)))));
+        printf("\n%d\n", (number % ((int)pow(10, (basamak - i - 1)))));
+        */
+        sum += ((number % ((int)pow(10, (basamak - i)))) - (number % ((int)pow(10, (basamak - i - 1))))) / (int)pow(10, (basamak - i - 1));
+
+        printf("%d\n", sum);
+
     
     }
 
