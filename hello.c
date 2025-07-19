ae8858cd0d249d0247cf11ecc4ec8c29e6e473c0
Kuzey T. Kale <166996695+kuzeytunakale@users.noreply.github.com>
2025-07-19 17:18:27 +0000

    2

--- hello.c
+++ hello.c
@@ -3,16 +3,14 @@
 
 int main() {
     int number = 1234, a = number, basamak = 0;
-    double ss = 0;
     while (a != 0) {
         a /= 10;
         basamak++;
     }
 
-    for (int i = 1; i <= basamak; i++)
-	{
-        ss = pow(10, (basamak - i));
-		printf("\n%lf\n", ss);
+    for (int i = 0; i < basamak; i++)
+	{ 
+        printf("\n%d\n", (number % ((int)pow(10, (basamak - i)))));
     
     }
 
