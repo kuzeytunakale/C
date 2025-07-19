0427fe4a4228f8f4a9e37e01c21ce412e713a017
Kuzey T. Kale <166996695+kuzeytunakale@users.noreply.github.com>
2025-07-19 11:22:12 +0000

    first committt

--- hello.c
+++ hello.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
+#include <math.h>
 
 int main() {
-    printf("Merhaba GitHub!\n");
+    int number = 1234, a = number, basamak = 0;
+    double ss = 0;
+    while (a != 0) {
+        a /= 10;
+        basamak++;
+    }
+
+    for (int i = 1; i <= basamak; i++)
+	{
+        ss = pow(10, (basamak - i));
+		printf("\n%lf\n", ss);
+    
+    }
+
     return 0;
 }
