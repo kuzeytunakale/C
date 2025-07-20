413eecbfef7c343307105fde618607039761aea4
Kuzey T. Kale <166996695+kuzeytunakale@users.noreply.github.com>
2025-07-20 19:34:45 +0000

    7777

--- DaireAlani.c
+++ DaireAlani.c
@@ -1,27 +1,37 @@
 #include<stdio.h>
+#include <stdlib.h>
 
-const PI = 3.1415926535;
-
-double pi(int deger)
+int main()
 {
-
-}
-
-void secim()
-{
+    const double PI = 3.1415926535;
     char cev_alan;
+    int r;
 
-    printf("Dairenin (c)evresini mi yoksa (a)lanını mı öğrenmek istersiniz?  :  ");
-    scanf("%d", &cev_alan);
+    printf("\nDairenin (c)evresini mi yoksa (a)lanını mı öğrenmek istersiniz?  :  ");
+    scanf("%c", &cev_alan);
+
+    if ((cev_alan != 'a') && (cev_alan != 'c')) {
+        printf("Anlaşılamayan bir değer girdiniz! Yeniden griniz.");
+        main();
+    }
+
+    printf("Yarıçapi kaç:  ");
+    scanf("%d", &r);
 
 
     switch (cev_alan){
         case 'c':
-            pi()
+            printf("Çevre: %lf\n", 2 * PI * r);
+            exit(0);
+            break;
+        
+        case 'a':
+            printf("Alanı: %lf\n", PI * r * r);
+            exit(0);
+            break;
+
     }
+
+    return 0;
 }
 
-int main() 
-{
-    secim();
-}
