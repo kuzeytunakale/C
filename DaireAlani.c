95b96b1d58a0bde2ee5db9619b6c07feb9e4ca94
Kuzey T. Kale <166996695+kuzeytunakale@users.noreply.github.com>
2025-07-20 18:36:32 +0000

    177

--- DaireAlani.c
+++ DaireAlani.c
@@ -1,1 +1,12 @@
+#include<stdio.h>
 
+int main() 
+{
+    const PI = 3.1415926535;
+    char cev_cap = 0;
+
+    printf("Dairenin (c)evresini mi yoksa (a)lanını mı öğrenmek istersiniz?  :  ");
+    scanf("%d", &cev_cap);
+    
+    shwich
+}
