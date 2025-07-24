0162e1bfb70495225c7f783aff92026a271fbae8
Kuzey T. Kale <166996695+kuzeytunakale@users.noreply.github.com>
2025-07-24 19:50:37 +0000

    66666554

--- DaireAlani.c
+++ DaireAlani.c
@@ -6,13 +6,16 @@ int main()
     const double PI = 3.1415926535;
     char cev_alan;
     int r;
+    goto aa;
 
+    aa:
+    cev_alan = 'm';
     printf("\nDairenin (c)evresini mi yoksa (a)lanını mı öğrenmek istersiniz?  :  ");
     scanf("%c", &cev_alan);
 
     if ((cev_alan != 'a') && (cev_alan != 'c')) {
         printf("Anlaşılamayan bir değer girdiniz! Yeniden griniz.");
-        main();
+        goto aa;
     }
 
     printf("Yarıçapi kaç:  ");
@@ -22,7 +25,7 @@ int main()
     switch (cev_alan){
         case 'c':
             printf("Çevre: %lf\n", 2 * PI * r);
-            exit(1);
+            exit(0);
             break;
         
         case 'a':
